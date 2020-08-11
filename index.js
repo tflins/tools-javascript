4b87f0a6e74ec1538c77d691e9c3166806765602
tflin <tflins@163.com>
2020-08-11 23:25:42 +0800

    update

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ export const stripHTMLTags = str => str.replace(/<[^>]*>/g, '')
 export const strEllipsis = (str, len = 100) =>
   str.length >= len ? `${str.slice(0, len)}'......` : str
 
+// 获取 [min, max] 间的随机整数
+export const getRandomInt = (min, max) => ~~(Math.random() * (max - min + 1) + min)
+
 // 是否为函数类型
 export const isFunction = func => typeof func === 'function'
 
@@ -90,4 +93,58 @@ export const once = function (el, event, fn) {
     off(el, event, listener)
   }
   on(el, event, listener)
+}
+
+// dom 是否有 class
+export function hasClass(el, cls) {
+  if (!el || !cls) return false
+  if (cls.indexOf(' ') !== -1)
+    throw new Error('className should not contain space.')
+  if (el.classList) {
+    return el.classList.contains(cls)
+  } else {
+    return (' ' + el.className + ' ').indexOf(' ' + cls + ' ') > -1
+  }
+}
+
+// 给 dom 添加 class
+export function addClass(el, cls) {
+  if (!el) return
+  var curClass = el.className
+  var classes = (cls || '').split(' ')
+
+  for (var i = 0, j = classes.length; i < j; i++) {
+    var clsName = classes[i]
+    if (!clsName) continue
+
+    if (el.classList) {
+      el.classList.add(clsName)
+    } else if (!hasClass(el, clsName)) {
+      curClass += ' ' + clsName
+    }
+  }
+  if (!el.classList) {
+    el.className = curClass
+  }
+}
+
+// dom 删除 class
+export function removeClass(el, cls) {
+  if (!el || !cls) return
+  var classes = cls.split(' ')
+  var curClass = ' ' + el.className + ' '
+
+  for (var i = 0, j = classes.length; i < j; i++) {
+    var clsName = classes[i]
+    if (!clsName) continue
+
+    if (el.classList) {
+      el.classList.remove(clsName)
+    } else if (hasClass(el, clsName)) {
+      curClass = curClass.replace(' ' + clsName + ' ', ' ')
+    }
+  }
+  if (!el.classList) {
+    el.className = trim(curClass)
+  }
 }
